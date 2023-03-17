e9e64ca19b6693f5787094042708bb6f9aa10161
jean dares <jeanborgesdares@gmail.com>
2023-03-17 18:58:10 -0300

    adicinando botão de editar

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -21,6 +21,11 @@ export class PensamentoService {
     return this.http.post<Pensamento>(this.API, pensamento)
   }
 
+  editar(pensamento: Pensamento): Observable<Pensamento> {
+    const url= `${this.API}/${pensamento.id}`
+    return this.http.put<Pensamento>(url, pensamento)
+  }
+
   excluir(id:number): Observable<Pensamento>{
     const url = `${this.API}/${id}`
     return  this.http.delete<Pensamento>(url)
@@ -31,3 +36,5 @@ export class PensamentoService {
     return this.http.get<Pensamento>(url)
   }
 }
+
+
